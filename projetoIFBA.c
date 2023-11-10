a8559e1a0cfc1f04e865b3a24f6c7966a4504341
João Victor <143547570+victorvlone@users.noreply.github.com>
2023-11-10 11:01:08 -0300

    Update projetoIFBA.c

--- projetoIFBA.c
+++ projetoIFBA.c
@@ -45,201 +45,240 @@ struct Disciplinas{
 
 int main () {
 
+
+    char respostaMENU[100];
     char resposta;
     int i = 0;
-    char SexoMasculino[] = "Masculino";
-    char SexoFeminino[] = "Feminino";
 
     struct Alunos Aluno[TAM];
     struct Professores Professor[TAM];
     struct Disciplinas Disciplina[TAM];
 
-    printf("CADASTRO DE ALUNOS\n\n");
+    system("cls || clear");
+    printf("         MENU\n\n");
+    printf("  Listas | Cadastros\n\n");
 
-    printf("Nome completo: ");
-    ler_texto(Aluno[i].NomeAluno, 1000);
-
-    printf("Sexo: ");
-    ler_texto(Aluno[i].SexoAluno, 10);
-    limpar_entrada();
-
-    Aluno[i].SexoAluno[0] = toupper(Aluno[i].SexoAluno[0]);
-    if (strcmp("Masculino", Aluno[i].SexoAluno) != 0 && strcmp("Feminino", Aluno[i].SexoAluno) != 0){
-        /* code */
-    }
-    
-
-    printf("DIA DE NASCIMENTO: ");
-    scanf("%d", &Aluno[i].DiadeNascimentoAluno);
-    limpar_entrada();
-    printf("MÊS DE NASCIMENTO: ");
-    scanf("%d", &Aluno[i].MesdeNascimentoAluno);
-    limpar_entrada();
-    printf("ANO DE NASCIMENTO: ");
-    scanf("%d", &Aluno[i].AnodeNascimentoAluno);
-    limpar_entrada();
-
-    printf("CPF(sem pontuaçâo): ");
-    scanf("%d", &Aluno[i].CPFAluno);
-    limpar_entrada();
-    
-    printf("Número de matrícula: ");
-    scanf("%d", &Aluno[i].NumerodeMatricula);
+    printf("Como deseja prosseguir? ");
+    ler_texto(respostaMENU, 100);
     limpar_entrada();
     system("cls || clear");
 
-    printf("Aperte a tecla 1 para cadastrar mais um\n");
-    printf("aluno ou a tecla 2 para cadastrar um professor:\n");
-    scanf("%c", &resposta);
-    system("cls || clear");
+    respostaMENU[0] = toupper(respostaMENU[0]);
 
-    while(resposta != '0'){
-        i = 1;
-        if(resposta == '1'){  
+    if (strcmp("Cadastros", respostaMENU) == 0){
 
-            do{
+        printf("1. Cadastrar Alunos\n");
+        printf("2. Cadastrar professores\n");
+        printf("3. Cadastrar disciplinas\n\n");
+        printf("O que deseja cadastrar primeiro? ");
+        scanf("%c", &resposta);
+        system("cls || clear");
 
-                printf("CADASTRO DE ALUNOS\n\n");
+        while(resposta != '0'){
 
-                printf("NOME COMPLETO: ");
-                ler_texto(Aluno[i].NomeAluno, 1000);
-                limpar_entrada();
+            if(resposta == '1'){  
 
-                printf("SEXO: ");
-                ler_texto(Aluno[i].SexoAluno, 10);
+                do{
 
-                printf("DATA DE NASCIMENTO\n");
-                printf("DIA: ");
-                scanf("%d", &Aluno[i].DiadeNascimentoAluno);
-                printf("MẼS: ");
-                scanf("%d", &Aluno[i].MesdeNascimentoAluno);
-                printf("ANO: ");
-                scanf("%d", &Aluno[i].AnodeNascimentoAluno);
+                    printf("CADASTRO DE ALUNOS\n\n");
 
-                printf("CPF(sem pontuaçâo): ");
-                scanf("%d", &Aluno[i].CPFAluno);
+                    printf("NOME COMPLETO: ");
+                    ler_texto(Aluno[i].NomeAluno, 1000);
+                    limpar_entrada();
 
-                printf("Número de matrícula: ");
-                scanf("%d", &Aluno[i].NumerodeMatricula);
-                system("cls || clear");
-                i++;
+                    printf("SEXO: ");
+                    ler_texto(Aluno[i].SexoAluno, 10);
 
-                if (i < TAM){
+                    Aluno[i].SexoAluno[0] = toupper(Aluno[i].SexoAluno[0]);
+                    while(strcmp("Masculino", Aluno[i].SexoAluno) != 0 && strcmp("Feminino", Aluno[i].SexoAluno) != 0){
 
-                    printf("Aperte a tecla 1 para cadastrar mais um\n");
-                    printf("aluno ou a tecla 2 para cadastrar um professor:\n");
-                    scanf("%c", &resposta);
+                        system("cls || clear");
+                        printf("Opção invalida! Tente novamente:\n");
+                        ler_texto(Aluno[i].SexoAluno, 10);
+                        system("cls || clear");
+                    
+                    }
+
+                    printf("DIA DE NASCIMENTO: ");
+                    scanf("%d", &Aluno[i].DiadeNascimentoAluno);
+
+                    while (Aluno[i].DiadeNascimentoAluno > 31 || Aluno[i].DiadeNascimentoAluno < 1){
+                        
+                        system("cls || clear");
+                        printf("invalido! Tente novamente:\n");
+                        scanf("%d", &Aluno[i].DiadeNascimentoAluno);
+                        system("cls || clear");
+
+                    }
+                    
+                    printf("MẼS DE NASCIMENTO: ");
+                    scanf("%d", &Aluno[i].MesdeNascimentoAluno);
+
+                    while (Aluno[i].MesdeNascimentoAluno > 12 || Aluno[i].MesdeNascimentoAluno < 1){
+                        
+                        system("cls || clear");
+                        printf("invalido! Tente novamente:\n");
+                        scanf("%d", &Aluno[i].MesdeNascimentoAluno);
+                        system("cls || clear");
+
+                    }
+
+                    printf("ANO DE NASCIMENTO: ");
+                    scanf("%d", &Aluno[i].AnodeNascimentoAluno);
+
+                    while (Aluno[i].AnodeNascimentoAluno > 2017 || Aluno[i].AnodeNascimentoAluno < 2006){
+                        
+                        system("cls || clear");
+                        printf("Não aceitamos alunos com menos de 7 anos ou com mais de 18.\n");
+                        sleep(2);
+                        system("cls || clear");
+                        printf("Deseja corrigir ou voltar ao menu?");
+                        ler_texto(respostaMENU, 100);
+
+                        respostaMENU[0] = toupper(respostaMENU[0]);
+                        if(strcmp("Voltar ao menu", respostaMENU) == 0){
+
+                            system("cls || clear");
+                            printf("         MENU\n\n");
+                            printf("  Listas | Cadastros\n\n");
+
+                            printf("Como deseja prosseguir? ");
+                            ler_texto(respostaMENU, 100);
+                            limpar_entrada();
+                            system("cls || clear");
+
+                            respostaMENU[0] = toupper(respostaMENU[0]);
+
+                            if (strcmp("Cadastros", respostaMENU) == 0){
+
+                                Aluno[i].AnodeNascimentoAluno = 2012;
+                                printf("1. Cadastrar Alunos\n");
+                                printf("2. Cadastrar professores\n");
+                                printf("3. Cadastrar disciplinas\n\n");
+                                printf("O que deseja cadastrar primeiro? ");
+                                scanf("%c", &resposta);
+                            }
+                        } else if(strcmp("Corrigir", respostaMENU) == 0){
+
+                            printf("ANO DE NASCIMENTO: ");
+                            scanf("%d", &Aluno[i].AnodeNascimentoAluno);
+
+                        }
+                    }
+
+                    printf("CPF(sem pontuaçâo): ");
+                    scanf("%d", &Aluno[i].CPFAluno);
+
+                    printf("Número de matrícula: ");
+                    scanf("%d", &Aluno[i].NumerodeMatricula);
+                    system("cls || clear");
+                    i++;
+
+                    if (i < TAM){
+
+                        printf("Aperte a tecla 1 para cadastrar mais um\n");
+                        printf("aluno ou a tecla 2 para cadastrar um professor:\n");
+                        scanf("%c", &resposta);
+                        system("cls || clear");
+
+                    }else{
+                        resposta = '2';
+                    }
+
+                } while(resposta == '1');
+
+            }else if (resposta == '2'){
+
+                do{
+    
+                    printf("CADASTRO DE PROFESSORES\n\n");
+
+                    printf("Nome completo: ");
+                    ler_texto(Professor[i].NomeProfessor, 1000);
                     system("cls || clear");
 
-                }else{
-                    resposta = '2';
-                }
+                    printf("CADASTRO DE PROFESSORES\n\n");
 
-            } while(resposta == '1');
+                    printf("Sexo: ");
+                    ler_texto(Professor[i].SexoProfessor, 10);
+                    system("cls || clear");
 
-        }else if (resposta == '2'){
+                    printf("CADASTRO DE PROFESSORES\n");
 
-            do{
- 
-                printf("CADASTRO DE PROFESSORES\n\n");
+                    printf("Data de nascimento\n\n");
+                    printf("Dia: ");
+                    scanf("%d", &Professor[i].DiadeNascimentoProfessor);
+                    printf("Mês: ");
+                    scanf("%d", &Professor[i].MesdeNascimentoProfessor);
+                    printf("Ano: ");
+                    scanf("%d", &Professor[i].AnodeNascimentoProfessor);
+                    system("cls || clear");
+                    
+                    printf("CADASTRO DE PROFESSORES\n\n");
 
-                printf("Nome completo: ");
-                ler_texto(Professor[i].NomeProfessor, 1000);
-                system("cls || clear");
+                    printf("CPF: ");
+                    scanf("%d", &Professor[i].CPFProfessor);
+                    system("cls || clear");
 
-                printf("CADASTRO DE PROFESSORES\n\n");
+                    printf("CADASTRO DE PROFESSORES\n\n");
 
-                printf("Sexo: ");
-                ler_texto(Professor[i].SexoProfessor, 10);
-                system("cls || clear");
+                    printf("Número de matrícula");
+                    scanf("%d", &Professor[i].NumerodeMatricula);
+                    system("cls || clear");
+                    i++;
 
-                printf("CADASTRO DE PROFESSORES\n");
+                    if (i < TAM){
 
-                printf("Data de nascimento\n\n");
-                printf("Dia: ");
-                scanf("%d", &Professor[i].DiadeNascimentoProfessor);
-                printf("Mês: ");
-                scanf("%d", &Professor[i].MesdeNascimentoProfessor);
-                printf("Ano: ");
-                scanf("%d", &Professor[i].AnodeNascimentoProfessor);
+                        printf("Aperte a tecla 2 para cadastrar mais um professor\n");
+                        printf("aperte a tecla 3 para cadastrar uma disciplina,\n");
+                        printf("ou aperte a tecla 0 para encerrar os cadastros:\n");
+                        scanf("%c", &resposta);
+                        system("cls || clear");
+
+                    }else{
+                        resposta = '3';
+                    }
+
+                } while(resposta == '2');
+
+            } else if (resposta == '3'){
+
+                do{
+                    printf("CADASTRO DE DISCIPLINAS\n\n");
+
+                    printf("Nome: ");
+                    ler_texto(Disciplina[i].NomedaDisciplina, 1000);    
+                    printf("Código: ");
+                    scanf("%d", &Disciplina[i].codigodaDisciplina);
+                    printf("Semestre: ");
+                    scanf("%d", &Disciplina[i].Semestre);
+                    printf("Professor: ");
+                    ler_texto(Disciplina[i].professor, 1000);
+
+                    if (i < TAM){
+
+                        printf("Aperte a tecla 3 para cadastrar mais um professor\n");
+                        printf("ou aperte a tecla 0 para encerrar os cadastros:\n");
+                        scanf("%c", &resposta);
+                        system("cls || clear");
+
+                    }else{
+                        resposta = '0';
+                    }
+
+                } while (resposta == '3');
+                
+            } else{
+
+                printf("Opção invalida! aperte a tecla 1 se deseja adicionar\n");
+                printf("mais um aluno, 2 para adicionar mais um professor, 3\n");
+                printf("para adicionar mais uma disciplina ou 0 para encerrar:\n");
+                scanf("%c", &resposta);
                 system("cls || clear");
                 
-                printf("CADASTRO DE PROFESSORES\n\n");
-
-                printf("CPF: ");
-                scanf("%d", &Professor[i].CPFProfessor);
-                system("cls || clear");
-
-                printf("CADASTRO DE PROFESSORES\n\n");
-
-                printf("Número de matrícula");
-                scanf("%d", &Professor[i].NumerodeMatricula);
-                system("cls || clear");
-                i++;
-
-                if (i < TAM){
-
-                    printf("Aperte a tecla 2 para cadastrar mais um professor\n");
-                    printf("aperte a tecla 3 para cadastrar uma disciplina,\n");
-                    printf("ou aperte a tecla 0 para encerrar os cadastros:\n");
-                    scanf("%c", &resposta);
-                    system("cls || clear");
-
-                }else{
-                    resposta = '3';
-                }
-
-            } while(resposta == '2');
-
-        } else if (resposta == '3'){
-
-            do{
-                printf("CADASTRO DE DISCIPLINAS\n\n");
-
-                printf("Nome: ");
-                ler_texto(Disciplina[i].NomedaDisciplina, 1000);    
-                printf("Código: ");
-                scanf("%d", &Disciplina[i].codigodaDisciplina);
-                printf("Semestre: ");
-                scanf("%d", &Disciplina[i].Semestre);
-                printf("Professor: ");
-                ler_texto(Disciplina[i].professor, 1000);
-
-                if (i < TAM){
-
-                    printf("Aperte a tecla 3 para cadastrar mais um professor\n");
-                    printf("ou aperte a tecla 0 para encerrar os cadastros:\n");
-                    scanf("%c", &resposta);
-                    system("cls || clear");
-
-                }else{
-                    resposta = '0';
-                }
-
-            } while (resposta == '3');
-            
-        } else{
-
-            printf("Opção invalida! aperte a tecla 1 se deseja adicionar\n");
-            printf("mais um aluno, 2 para adicionar mais um professor, 3\n");
-            printf("para adicionar mais uma disciplina ou 0 para encerrar:\n");
-            scanf("%c", &resposta);
-            system("cls || clear");
-            
+            }
         }
-
     }
-
-
-    printf("1. LISTA DE ALUNOS\n");
-    printf("2. LISTA DE PROFESSORES\n");
-    printf("3. LISTA DE DISCIPLINAS\n");
-    printf("4. ");
-    for (i = 0; i < TAM; i++){
-        printf("%s\n", &Aluno[i].NomeAluno);
-    }
-    
-
 
     return 0;
 }
